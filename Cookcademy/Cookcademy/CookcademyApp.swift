c1e4c7e6ad2921cff26e6603148ecc14b8434e06
dwange <96422553+dwange@users.noreply.github.com>
2024-05-20 12:31:51 +0300

    initial commit

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,19 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Ekaterina Savina
+//
+
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+           MainTabView()
+        }
+    }
+}
+
